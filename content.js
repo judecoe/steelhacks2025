10ccd882c4d02371782b6c0fbc7cf716964cfc70
kiermurr <kfm41@pitt.edu>
2025-09-20 19:06:08 -0400

    shipping and price selectors fix

--- content.js
+++ content.js
@@ -850,6 +850,7 @@ function extractItemPageCosts() {
 
   // Look for price on item page
   const priceSelectors = [
+    "span.ux-textspans",
     ".price-current",
     ".display-price",
     ".notranslate",
@@ -872,6 +873,7 @@ function extractItemPageCosts() {
 
   // Look for shipping on item page
   const shippingSelectors = [
+    "span.class=\"ux-textspans ux-textspans--POSITIVE ux-textspans--BOLD\"",
     "[class*='shipping']",
     "[class*='delivery']",
     "[id*='shipping']",
