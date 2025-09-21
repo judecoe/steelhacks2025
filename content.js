5bb3bdaa818e8f85a7718b88bf231223383d6912
kiermurr <kfm41@pitt.edu>
2025-09-21 15:44:27 -0400

    test

--- content.js
+++ content.js
@@ -48,7 +48,7 @@ function injectPokePriceStyles() {
     .pokeprice-item-page-container {
       position: absolute !important;
       top: 162px;
-      right: 200px;
+      right: 180px;
       z-index: 1000;
       display: flex;              /* line up children horizontally */
       align-items: center;        /* vertically center them */
