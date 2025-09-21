4ab50d3e917f9e3ed831f821b69bcea25f26012e
kiermurr <kfm41@pitt.edu>
2025-09-21 15:36:55 -0400

    test

--- content.js
+++ content.js
@@ -55,12 +55,19 @@ function injectPokePriceStyles() {
       justify-content: flex-end;  /* push both to the right side */
     }
     .pokeprice-item-page-container .pokeprice-tag {
+      padding: 5px 10px;
+      font-size: 15px;
+      font-weight: bold;
+      border-radius: 6px;
       order: 1;
       position: relative !important;
       right: auto;
       bottom: auto;
     }
     .pokeprice-item-page-container .pokeprice-cost-box {
+      padding: 5px 10px;
+      font-size: 15px;
+      border-radius: 6px;
       order: 2;
       position: relative !important;
       right: auto;
